f392605aa7e458f23f1953a5d2737b0fff226898
Motaz <motazyusuf@gmail.com>
2024-07-30 22:12:15 +0300

    With temp list

--- lib/modules/quran/widgets/quran_details_view.dart
+++ lib/modules/quran/widgets/quran_details_view.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:islami_app/modules/quran/widgets/sura_title_widget.dart';
 
-class QuranDetailsView extends StatelessWidget {
+class QuranDetailsView extends StatefulWidget {
   const QuranDetailsView({super.key});
 
   static const String routeName = "quranDetails";
 
   @override
+  State<QuranDetailsView> createState() => _QuranDetailsViewState();
+}
+
+class _QuranDetailsViewState extends State<QuranDetailsView> {
+  @override
   Widget build(BuildContext context) {
     var data = ModalRoute.of(context)?.settings.arguments as SuraTitleWidget;
+
+    if (content.isEmpty) loadData(data.number);
 
     return Container(
       decoration: const BoxDecoration(
@@ -46,11 +54,34 @@ class QuranDetailsView extends StatelessWidget {
               ),
               const Divider(
                 height: 10,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index) => Text(
+                    "(${index + 1}) ${versesList[index]} ",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium
+                        ?.copyWith(height: 1.8),
+                    textAlign: TextAlign.center,
+                  ),
+                  itemCount: versesList.length,
+                ),
               )
             ],
           ),
         ),
       ),
     );
+  }
+
+  String content = "";
+  List<String> versesList = [];
+
+  Future<void> loadData(String suraNumber) async {
+    content = await rootBundle.loadString("assets/files/$suraNumber.txt");
+    versesList = content.split("\n");
+    print(content);
+    setState(() {});
   }
 }
