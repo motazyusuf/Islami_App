4d206aabbe9a758952dab9f03582d9178b747e97
Motaz <motazyusuf@gmail.com>
2024-07-31 18:28:08 +0300

    -Hadeth view -Hadeth details view

--- lib/modules/hadeth/hadeth_view.dart
+++ lib/modules/hadeth/hadeth_view.dart
@@ -1,19 +1,85 @@
 import 'package:flutter/material.dart';
-
-import '../../layouts/layout_view.dart';
+import 'package:islami_app/modules/hadeth/widgets/ahadeth_title.dart';
+import 'package:islami_app/modules/hadeth/widgets/hadeth_details_view.dart';
 
 class HadethView extends StatelessWidget {
   static const String routeName = "hadeth";
 
-  const HadethView({super.key});
+  List<String> ahadith = [
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+    "حديث رقم",
+  ];
+
+  HadethView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "This is $routeName",
-        style: Theme.of(context).textTheme.titleLarge,
-      ),
+    return Column(
+      children: [
+        Image.asset("assets/images/HadithViewImage.png"),
+        const AhadethTitle(),
+        Expanded(
+            child: InkWell(
+          onTap: () {
+            Navigator.pushNamed(context, HadethDetailsView.routeName);
+          },
+          child: ListView.builder(
+            itemBuilder: (context, index) => Text(
+              "${ahadith[index]} ${index + 1}",
+              textAlign: TextAlign.center,
+            ),
+            itemCount: ahadith.length,
+          ),
+        ))
+      ],
     );
   }
 }
---
+++ lib/modules/hadeth/widgets/hadeth_details_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HadethDetailsView extends StatelessWidget {
+  HadethDetailsView({super.key});
+
+  static const String routeName = "hadethDetailsView";
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
