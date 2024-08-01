81c4b7a562df3bcf3a968ffc52006509188e4fbb
Motaz <motazyusuf@gmail.com>
2024-08-01 12:14:38 +0300

    Hadith index written

--- lib/modules/hadeth/hadeth_view.dart
+++ lib/modules/hadeth/hadeth_view.dart
@@ -1,70 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:islami_app/modules/hadeth/widgets/ahadeth_title.dart';
 import 'package:islami_app/modules/hadeth/widgets/hadeth_details_view.dart';
 
-class HadethView extends StatelessWidget {
+class HadethView extends StatefulWidget {
   static const String routeName = "hadeth";
-
-  List<String> ahadith = [
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-    "حديث رقم",
-  ];
 
   HadethView({super.key});
 
   @override
+  State<HadethView> createState() => _HadethViewState();
+}
+
+class _HadethViewState extends State<HadethView> {
+  @override
   Widget build(BuildContext context) {
+    if (content.isEmpty) loadData();
     return Column(
       children: [
-        Image.asset("assets/images/HadithViewImage.png"),
+        Image.asset(
+          "assets/images/HadithViewImage.png",
+          scale: 3,
+        ),
         const AhadethTitle(),
         Expanded(
             child: InkWell(
@@ -72,14 +29,42 @@ class HadethView extends StatelessWidget {
             Navigator.pushNamed(context, HadethDetailsView.routeName);
           },
           child: ListView.builder(
-            itemBuilder: (context, index) => Text(
-              "${ahadith[index]} ${index + 1}",
-              textAlign: TextAlign.center,
+            itemBuilder: (context, index) => Container(
+              padding: EdgeInsetsDirectional.only(top: 5),
+              child: Text(
+                "${ahadithFinalList[index].title}",
+                textAlign: TextAlign.center,
+              ),
             ),
-            itemCount: ahadith.length,
+            itemCount: ahadithFinalList.length,
           ),
         ))
       ],
     );
   }
+
+  List<Hadith> ahadithFinalList = [];
+  String content = "";
+
+  Future<void> loadData() async {
+    content = await rootBundle.loadString("assets/files/ahadeth.txt");
+
+    List<String> ahadithList = content.split("#");
+
+    for (int i = 0; i < ahadithList.length; i++) {
+      String hadithInstance = ahadithList[i].trim();
+      int titleLength = hadithInstance.indexOf("\n");
+      String title = hadithInstance.substring(0, titleLength);
+      String body = hadithInstance.substring(titleLength + 1);
+      Hadith hadith = Hadith(title: title, body: body);
+      ahadithFinalList.add(hadith);
+      setState(() {});
+    }
+  }
+}
+
+class Hadith {
+  String title, body;
+
+  Hadith({required this.title, required this.body});
 }
--- lib/modules/hadeth/widgets/hadeth_details_view.dart
+++ lib/modules/hadeth/widgets/hadeth_details_view.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:islami_app/layouts/detailes_views_layout.dart';
 
-class HadethDetailsView extends StatelessWidget {
+class HadethDetailsView extends StatefulWidget {
   HadethDetailsView({super.key});
 
   static const String routeName = "hadethDetailsView";
 
   @override
+  State<HadethDetailsView> createState() => _HadethDetailsViewState();
+}
+
+class _HadethDetailsViewState extends State<HadethDetailsView> {
+  @override
   Widget build(BuildContext context) {
+    var data = ModalRoute.of(context)?.settings.arguments as Text;
+
     return DetailesViewsLayout(
         child: const Column(
       children: [
