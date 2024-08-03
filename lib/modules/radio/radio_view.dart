3849c4e24d657d732c77195d5b8e97ba12040a34
Motaz <motazyusuf@gmail.com>
2024-08-03 13:29:50 +0300

    Radio view

--- lib/modules/radio/radio_view.dart
+++ lib/modules/radio/radio_view.dart
@@ -1,12 +1,62 @@
 import 'package:flutter/material.dart';
 
-class RadioView extends StatelessWidget {
+class RadioView extends StatefulWidget {
   static const String routeName = "radio";
 
-  const RadioView({super.key});
+  RadioView({super.key});
+
+  @override
+  State<RadioView> createState() => _RadioViewState();
+}
+
+class _RadioViewState extends State<RadioView> {
+  bool isPressed = false;
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Image.asset("assets/images/RadioViewImage.png"));
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+            margin: EdgeInsetsDirectional.only(bottom: 15),
+            child: Image.asset("assets/images/RadioViewImage.png")),
+        Text("اذاعة القرآن الكريم"),
+        SizedBox(
+          height: 90,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              color: Color.fromRGBO(250, 204, 29, 1),
+              iconSize: 60,
+              icon: const Icon(Icons.skip_next),
+              onPressed: () {
+                // ...
+              },
+            ),
+            IconButton(
+              color: Color.fromRGBO(250, 204, 29, 1),
+              iconSize: 100,
+              icon: isPressed
+                  ? const Icon(Icons.pause)
+                  : const Icon(Icons.play_arrow),
+              onPressed: () async {
+                isPressed = !isPressed;
+                setState(() {});
+              },
+            ),
+            IconButton(
+              color: Color.fromRGBO(250, 204, 29, 1),
+              iconSize: 60,
+              icon: const Icon(Icons.skip_previous),
+              onPressed: () {
+                // ...
+              },
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
